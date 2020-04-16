b2d6c9cb4cc38cf427a735cf29a30d1c67067f7b
Oscar Mier <voscarmv@gmail.com>
2020-04-16 11:26:19 -0500

    Flash messages for destroy and create.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,12 +15,14 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice = " Article '#{@article.title}' created 🐣!"
     redirect_to articles_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+    flash.notice = "Article '#{@article.title}'... Destroyed 💀"
     redirect_to articles_path
   end
 
@@ -33,7 +35,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
-    flash.notice = " Article '#{@article.title}' updated!"
+    flash.notice = " Article '#{@article.title}' updated 🩹!"
     redirect_to article_path(@article)
   end
 end
