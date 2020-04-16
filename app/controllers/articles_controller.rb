1570e974fbdaf4373c10d743c1d7ce5cadb88e89
Oscar Mier <voscarmv@gmail.com>
2020-04-16 11:10:50 -0500

    Added edit and update.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(articles_params)
+    @article = Article.new(article_params)
     @article.save
     redirect_to articles_path(@article)
   end
@@ -22,5 +22,17 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  def edit    
+    @article = Article.find(params[:id])
+    # @article.update(article_params)
+    # redirect_to articles_path
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
   end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,5 @@
 module ArticlesHelper
-  def articles_params
+  def article_params
     params.require(:article).permit(:title, :body)
   end
 end
