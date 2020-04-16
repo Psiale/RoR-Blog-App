c3b97896cc3c236274aa8d7d0d80dad8c2f17b4a
psialedev <psialedev@gmail.com>
2020-04-16 11:19:08 -0500

    added flash to the edit action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,6 +33,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
+    flash.notice = " Article '#{@article.title}' updated!"
     redirect_to article_path(@article)
   end
 end
