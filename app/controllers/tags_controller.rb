5fde1c3103bf8f3524a0d6c82f653c690abc1bc2
Oscar Mier <voscarmv@gmail.com>
2020-04-17 12:16:13 -0500

    Added index to tags and tag cascade deletion

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,13 @@
 class TagsController < ApplicationController
+    def index
+        @tags = Tag.all
+    end
+    def destroy
+        @tag = Tag.find(params[:id])
+        @tag.destroy
+        flash.notice = "Tag '#{@tag.title}'... Destroyed 💀"
+        redirect_to tags_path
+    end
     def show
         @tag = Tag.find(params[:id])
     end
